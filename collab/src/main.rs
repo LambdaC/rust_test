d3027066132636af8edc8dca7b339fc537cb1be1
LambdaC <studycwg@gmail.com>
2022-05-26 14:42:46 +0800

    simple server return json data

--- collab/src/main.rs
+++ collab/src/main.rs
@@ -1,25 +1,26 @@
 /**
- * The first two steps for each framework are the same:
-    • Start a server on a specific port
-    • Provide route handler functions to an incoming HTTP request, which matches the
-    path, HTTP method and parameter specified
- */
+* The first two steps for each framework are the same:
+   • Start a server on a specific port
+   • Provide route handler functions to an incoming HTTP request, which matches the
+   path, HTTP method and parameter specified
+*/
 
 /**
- * Every time a HTTP request comes in, the framework is 
-processing it in a few steps: 
-    • Check the request path inside the HTTP request 
-    • Check the HTTP request type (GET, PUT, POST etc.) 
-    • Forward the request to a route handler which is responsible for the path and type 
-    • Before forwarding it to the end route handler, the request can be passed through a 
-    so-called middleware which checks things like authentication headers or adds further 
-    information to the request object for the end route handler 
+ * Every time a HTTP request comes in, the framework is
+processing it in a few steps:
+    • Check the request path inside the HTTP request
+    • Check the HTTP request type (GET, PUT, POST etc.)
+    • Forward the request to a route handler which is responsible for the path and type
+    • Before forwarding it to the end route handler, the request can be passed through a
+    so-called middleware which checks things like authentication headers or adds further
+    information to the request object for the end route handler
  */
-
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+use warp::Filter;
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -27,7 +28,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -50,10 +51,7 @@ impl FromStr for QuestionId {
     }
 }
 
-use warp::Filter;
-
-#[tokio::main]
-async fn main() {
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
@@ -62,11 +60,28 @@ async fn main() {
     );
     println!("{:?}", question);
 
+   Ok(warp::reply::json(&question))
+}
+
+
+#[tokio::main]
+async fn main() {
+
     // create a path Filter
     let hi = warp::path("hello").map(|| {
         println!("request received");
         format!("Hello, World!")
     });
 
-    warp::serve(hi).run(([127, 0, 0, 1], 3030)).await;
+    // only handle get request
+    let hi = warp::get().map(|| format!("Hello"));
+
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
